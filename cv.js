22096b89f137c9755e0ab9bb80d3bfb41eca00ef
Svetlana Pozina <cherryblack@list.ru>
2022-03-29 04:47:51 +0300

    for mobile

--- cv.js
+++ cv.js
@@ -6,11 +6,32 @@ document.addEventListener( 'click', (e) => {
     pict.forEach((Element) => 
     {
     if ((!(e.composedPath().includes(Element)))||(Element.style.transform != '')) Element.style.transform = '';
-    else Element.style.transform = 'scale(3)';
+
+    else {
+
+    if (Element.style.height === '8em')  {Element.style.transform = 'scale(3)'; Element.style.position = 'absolute'; Element.style.left = '0'; 
+    Element.style.right = '0'; Element.style.margin = 'auto';}  
+
+    else {Element.style.transform = 'scale(3)';Element.style.position = ''; Element.style.left = ''; 
+    Element.style.right = ''; Element.style.margin = '0.2em';}
+
+    }
+
     }
     );
 })
 
+
+// document.addEventListener( 'click', (e) => {
+
+//     pict.forEach((Element) => 
+//     {
+//     if ((!(e.composedPath().includes(Element)))||(Element.style.transform != '')) Element.style.transform = '';
+//     else {Element.style.transform = 'scale(3)'; Element.style.position = 'absolute'; Element.style.left = '0'; 
+//     Element.style.right = '0'; Element.style.margin = 'auto';}
+//     }
+//     );
+// })
 
 // for (let i in pict) {
 

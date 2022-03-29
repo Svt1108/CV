7a40f38140fb4550d2d0963474229fc9e55481e4
Svetlana Pozina <cherryblack@list.ru>
2022-03-29 05:11:54 +0300

    end of test

--- cv.js
+++ cv.js
@@ -6,17 +6,7 @@ document.addEventListener( 'click', (e) => {
     pict.forEach((Element) => 
     {
     if ((!(e.composedPath().includes(Element)))||(Element.style.transform != '')) Element.style.transform = '';
-
-    else {
-
-    if (window.matchMedia("only screen and (max-width: 750px)").matches)  {Element.style.transform = 'scale(3)'; Element.style.position = 'absolute'; Element.style.left = '0'; 
-    Element.style.right = '0'; Element.style.margin = 'auto';}  
-
-    else {Element.style.transform = 'scale(3)';Element.style.position = ''; Element.style.left = ''; 
-    Element.style.right = ''; Element.style.margin = '0.2em';}
-
-    }
-
+    else Element.style.transform = 'scale(3)';
     }
     );
 })
@@ -27,8 +17,17 @@ document.addEventListener( 'click', (e) => {
 //     pict.forEach((Element) => 
 //     {
 //     if ((!(e.composedPath().includes(Element)))||(Element.style.transform != '')) Element.style.transform = '';
-//     else {Element.style.transform = 'scale(3)'; Element.style.position = 'absolute'; Element.style.left = '0'; 
-//     Element.style.right = '0'; Element.style.margin = 'auto';}
+
+//     else {
+
+//     if (window.matchMedia("only screen and (max-width: 750px)").matches)  {Element.style.transform = 'scale(3)'; Element.style.position = 'absolute'; Element.style.left = '0'; 
+//     Element.style.right = '0'; Element.style.margin = 'auto';}  
+
+//     else {Element.style.transform = 'scale(3)';Element.style.position = ''; Element.style.left = ''; 
+//     Element.style.right = ''; Element.style.margin = '0.2em';}
+
+//     }
+
 //     }
 //     );
 // })
